208865565b1fc052df0ad731fae5f412b7dbf235
yash <yashasvikbhardwaj@gmail.com>
2023-08-04 00:33:38 +0530

    Initial commit

---
+++ js/questions.js
@@ -0,0 +1,224 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which alphabet is shown in the image?",
+    answer: "T",
+    options: [
+      "A",
+      "R",
+      "T",
+      "Y"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which number is shown in the image?",
+    answer: "4",
+    options: [
+      "2",
+      "4",
+      "6",
+      "7"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which weekday is shown in the image?",
+    answer: "Saturday",
+    options: [
+      "Sunday",
+      "Tuesday",
+      "Thursday",
+      "Saturday"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which month is shown in the image?",
+    answer: "May",
+    options: [
+      "March",
+      "May",
+      "July",
+      "September"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which word is shown in the image?",
+    answer: "This",
+    options: [
+      "This",
+      "That",
+      "Answer",
+      "Place"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which alphabet is shown in the image?",
+    answer: "N",
+    options: [
+      "N",
+      "K",
+      "S",
+      "P"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which word is shown in the image?",
+    answer: "Apartment",
+    options: [
+      "Good",
+      "Camera",
+      "Home",
+      "Apartment"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which number is shown in the image?",
+    answer: "100",
+    options: [
+      "100",
+      "1000",
+      "25",
+      "50"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which word is shown in the image?",
+    answer: "Again",
+    options: [
+      "Again",
+      "Work",
+      "Sad",
+      "Happy"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which shape is shown in the image",
+    answer: "Triangle",
+    options: [
+      "Triangle",
+      "Square",
+      "Oval",
+      "Circle"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Which alphabet is shown in the image?",
+    answer: "I",
+    options: [
+      "S",
+      "T",
+      "I",
+      "J"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Which number is shown in the image?",
+    answer: "8",
+    options: [
+      "15",
+      "12",
+      "9",
+      "8"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Which month is shown in the image?",
+    answer: "February",
+    options: [
+      "June",
+      "July",
+      "August",
+      "February"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Which word is shown in the image?",
+    answer: "Car",
+    options: [
+      "Car",
+      "Correct",
+      "Wrong",
+      "Again"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Which weekday is shown in the image?",
+    answer: "Monday",
+    options: [
+      "Monday",
+      "Wednesday",
+      "Friday",
+      "Saturday"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Which month is shown in the image?",
+    answer: "December",
+    options: [
+      "December",
+      "November",
+      "July",
+      "October"
+    ]
+  },
+  {
+    numb: 17,
+    question: "Which word is shown in the image?",
+    answer: "When",
+    options: [
+      "Which",
+      "When",
+      "What",
+      "Why"
+    ]
+  },
+  {
+    numb: 18,
+    question: "Which colour is shown in the image?",
+    answer: "Brown",
+    options: [
+      "Brown",
+      "Black",
+      "Yellow",
+      "Blue"
+    ]
+  },
+  {
+    numb: 19,
+    question: "Which word is shown in the image?",
+    answer: "Work",
+    options: [
+      "This",
+      "Work",
+      "Language",
+      "Table"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Which weekday is shown in the image?",
+    answer: "Thursday",
+    options: [
+      "Tuesday",
+      "Sunday",
+      "Friday",
+      "Thursday"
+    ]
+  },
+
+];
